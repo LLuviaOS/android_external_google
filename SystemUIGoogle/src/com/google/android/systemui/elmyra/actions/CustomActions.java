3f44d34374729230ac15c9e650fa60a72efbb8d8
squabbi <squabbi.doge@gmail.com>
2020-03-21 12:57:01 +1100

    Active Edge & Gestures nav: Add Skip/Previous Song Function [3/3]
    Change-Id: I4d5726cf8baf681b52b64ede1412cf842d3cea4e
    Signed-off-by: Ram9199 <sriramscience7@gmail.com>

--- SystemUIGoogle/src/com/google/android/systemui/elmyra/actions/CustomActions.java
+++ SystemUIGoogle/src/com/google/android/systemui/elmyra/actions/CustomActions.java
@@ -9,6 +9,7 @@ import android.os.UserHandle;
 import android.provider.MediaStore;
 import android.provider.Settings;
 import android.text.TextUtils;
+import android.view.KeyEvent;
 
 import com.android.internal.util.lluvia.lluviaUtils;
 import com.android.systemui.Dependency;
@@ -101,6 +102,12 @@ public class CustomActions extends Action {
                 if (isScreenOn) {
                     lluviaUtils.killForegroundApp();
                 }
+                break;
+            case 14: // Skip song
+                lluviaUtils.sendSystemKeyToStatusBar(KeyEvent.KEYCODE_MEDIA_NEXT);
+                break;
+            case 15: // Previous song
+                lluviaUtils.sendSystemKeyToStatusBar(KeyEvent.KEYCODE_MEDIA_PREVIOUS);
                 break;
         }
     }

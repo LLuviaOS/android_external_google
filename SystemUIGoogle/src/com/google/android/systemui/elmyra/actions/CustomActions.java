ezio84/abc_external_google

Add kill app action to squeeze actions [3/3]

SystemUIGoogle/src/com/google/android/systemui/elmyra/actions/CustomActions.java
@@ -97,6 +97,9 @@ public class CustomActions extends Action {
             case 12: // Ringer modes
                 Utils.toggleRingerModes(getContext());
                 break;
+            case 13: // Kill app
+                Utils.killForegroundApp();
+                break;
         }
     }
 

LLuviaOS/android_external_google

Change-Id: I664c731b276293f3e0e0b94042e62db02ed9b550
Signed-off-by: Ram9199 <sriramscience7@gmail.com>

SystemUIGoogle/src/com/google/android/systemui/elmyra/actions/CustomActions.java
@@ -97,6 +97,11 @@ public class CustomActions extends Action {
             case 12: // Ringer modes
                 lluviaUtils.toggleRingerModes(getContext());
                 break;
+            case 13: // Kill app
+                if (isScreenOn) {
+                    lluviaUtils.killForegroundApp();
+                }
+                break;
         }
     }
 